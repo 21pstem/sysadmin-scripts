d7dd76d7f940c554efd16a46765b7d4dc06392a5
waleo <writewale@gmail.com>
2014-02-18 18:26:51 -0500

    initial version, there is a defect - a file named 2 is being created

---
+++ production_db_backup/README.md
@@ -0,0 +1,1 @@
+* Do not include the exact location of the files we use in this script, since it's public
---
+++ production_db_backup/db_backup.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -x
+
+umask 077
+max_backup_keep=2
+
+function do_db_backup() {
+    config_file=$1
+    backup_path=$2
+    environment=$3
+
+    #gets a field from database.yml
+    function get_field () {
+      line=`grep $1 $config_file` #grep the line
+      value=${line##*:} #get the value
+      value=`echo $value | tr -d ''` #remove all white spaces
+      echo $value
+    }
+
+    function trim_backups {
+	arch_files=(`ls -rt $backup_path`) #reverse list of files in an array
+        count=${#arch_files[@]}
+	if [ $count > $max_backup_keep ]
+	   then
+               del_count=`expr $count - $max_backup_keep`
+	       del_count=`expr $del_count - 1` #normalize for zero based array
+               for i in $(seq 0 $del_count)
+                 do
+                   rm $backup_path/${arch_files[$i]}
+                 done
+        fi
+    }
+    
+    pwd=`get_field password`
+    databse=`get_field database`
+    user=`get_field username`
+    database=`get_field database`
+
+    date=`date '+%Y-%m-%d_%H:%M:%S'`
+    backup_file=database_${environment}_${database}_$date.bz2
+
+    /usr/bin/mysqldump -u $user --password=$pwd --databases $database | /usr/bin/bzip2 -9 > $backup_path/$backup_file
+    trim_backups
+}
+
+### DO  ####
+do_db_backup database.yml backups_location custom_envname
+
+
